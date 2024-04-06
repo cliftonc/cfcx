7864b6d191b1eb2ea48444f62bb8e5d7fcf942d8
cliftonc <clifton.cunningham@gmail.com>
2024-04-06 11:55:22 +0200

    Sort of working hacked version

--- src/index.js
+++ src/index.js
@@ -5,49 +5,66 @@ const htmlEntities = require('html-entities');
 export default {
   async fetch(request, env, ctx) {
 
-    function getCxAttr(node, name, defaultAttr) {
-      var value = node.attribs[name] || node.attribs['data-' + name];
-      if (value) {
-        return htmlEntities.decode(value);
+    let url = new URL(request.url)    
+    if(url.pathname === '/favicon.ico') {
+      return new Response('');
+    }
+
+    if(url.pathname === '/') {
+
+      function getCxAttr(node, name, defaultAttr) {
+        var value = node.attribs[name] || node.attribs['data-' + name];
+        if (value) {
+          return htmlEntities.decode(value);
+        }
+        return defaultAttr === undefined ? false : defaultAttr;
       }
-      return defaultAttr === undefined ? false : defaultAttr;
-    }
 
-    const getCx = (fragment, next) => {
-      var cxUrl = getCxAttr(fragment, 'cx-url');
-      // Here we would actually go and get the content / cache it etc.
-      return next(null, cxUrl);
-    }
+      const getCx = (fragment, next) => {
+        var cxUrl = getCxAttr(fragment, 'cx-url');
+        // Fetch honors cache headers automagically
+        fetch(cxUrl, {
+          headers: {
+            "X-Source": "Cloudflare-Workers",
+          },
+        }).then((resp) => {        
+          resp.text().then((content) => {
+            return next(null, content)
+          })
+        });
+      }
 
-    // Config should come via the Backends
-    const config = {
-      showErrors: true,
-      plugins: [
-        parxerPlugins.Url(getCx)
-      ]
-    };
+      // Config should come via the Backends eventually
+      const config = {
+        showErrors: true,
+        plugins: [
+          parxerPlugins.Url(getCx)
+        ]
+      };
 
-    // Super simple fixed parse
-    const parse = () => {
-      return new Promise((resolve, reject) => {
-        // Content should come via the backend
-        let input = `<html>
-          <h1>HELLO WORLD!</h1>
-          <p>The following come via parsing of "cx-url" attributes dynamically:</p>
-          <div cx-url='https://www.google.com'></div>
-          <div cx-url='https://www.infinitaslearning.com'></div>
-        </html>`;
-        parxer(config, input, function(err, fragmentCount, data) {
-          if (err.fragmentErrors) resolve(err)
-          else resolve(data);
+      // Super simple fixed parse
+      const parse = () => {
+        return new Promise((resolve, reject) => {
+          // Content should come via the backend
+          let input = `<html>
+            <h1>HELLO WORLD!</h1>
+            <p>The following come via parsing of "cx-url" attributes dynamically:</p>            
+            <div cx-url='https://www.example.com'></div>          
+          </html>`;
+          parxer(config, input, function(err, fragmentCount, data) {
+            if (err.fragmentErrors) resolve(err)
+            else resolve(data);
+          })
         })
-      })
-    }
-    let newResp = new Response(await parse(), {
-      headers: {
-        "Content-Type": "text/html"
       }
-    })
-    return newResp;
+      let newResp = new Response(await parse(), {
+        headers: {
+          "Content-Type": "text/html"
+        }
+      })
+      return newResp;
+    }
+
+    return new Response('');
   },
 };
